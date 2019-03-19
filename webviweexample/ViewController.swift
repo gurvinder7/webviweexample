b67902b049606cd31dfc7a1b33aee44fc004b993
gurvinder mangat <gurvindermangat@gurvinders-MacBook-Pro.local>
2019-03-19 18:37:17 -0400

    web view

--- webviweexample/ViewController.swift
+++ webviweexample/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadFromString()
+        //loadfromWeb()
+       // loadurl()
+       
+        
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func loadfromWeb()   {
+         let url = URL(string: "https://www.google.com")
+        
+        let urlReq = URLRequest(url: url!)
+        myWebView.load(urlReq)
+    }
+    func loadFromString() {
+        let htmlStr = "<h1>hello World</h1>"
+        myWebView.loadHTMLString(htmlStr, baseURL: nil)
+        
+    }
+    func loadurl(){
+         let localfilePath = Bundle.main.url(forResource: "home", withExtension: "html")
+        // let url = URL(string: "https://www.google.com")
+        
+        let urlReq = URLRequest(url: localfilePath!)
+        myWebView.load(urlReq)
+        
+    }
 
 }
 
